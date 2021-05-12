750a0ee5d70339c54f38948b27f08c96a291f9a2
B3xis <atatanishe@gmail.com>
2021-05-12 20:03:32 +0300

    12

---
+++ Molot_Tora/Assets/Scripts/MooveScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MooveScript : MonoBehaviour
+{
+    public Player player;
+  
+    void OnMouseDrag()
+    {
+
+        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //player.transform.position = Vector3.MoveTowards(player.transform.position,  new Vector2(mousePosition.x, player.transform.position.y), 0.2f);
+
+        // Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector3 mousePosition  = Input.mousePosition;
+        //mousePosition.x = mousePosition.x > 5f ? 5f : mousePosition.x;
+        //mousePosition.x = mousePosition.x < -5f ? -5f : mousePosition.x;
+        //player.transform.position = new Vector2(mousePosition.x, this.transform.position.y);
+
+
+        // Vector2 mousePosition = new Vector2(Input.mousePosition.x, player.position.y); // переменной записываютьс€ координаты мыши по иксу и игрику
+        // Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваетьс€ переменна€ с координатами мыши
+        // player.position = objPosition; // и собственно объекту записываютьс€ координаты  
+
+
+
+        //Vector2 targetPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //targetPos.y = 0; // I fixed Y value to zero you may change this to value you want
+        //player.position = targetPos;
+        //if ((Vector2)transform.position != targetPos)
+        //{
+        //    player.position = Vector2.MoveTowards(player.position, targetPos, speed * Time.deltaTime);
+        //
+        //}
+
+        //if (Input.mousePosition.x > Screen.width / 2)
+        //    player.GetComponent<Rigidbody>().MovePosition(new Vector3(Input.mousePosition.x / 100000, 1, 0));
+        //else
+        //    player.GetComponent<Rigidbody>().MovePosition(new Vector3(-(Screen.width - Input.mousePosition.x) / 100000, 1, 0));
+        if (Input.mousePosition.x > Screen.width / 2)
+        {
+
+
+            if (player.transform.position.x <= 4.7f)
+            {
+                if (player.transform.position.x >= 5)
+                {
+                    player.transform.position = new Vector3(5, 0, 0);
+
+                }
+                // player.transform.Rotate(0,-0.1f,0);
+                player.transform.Translate(new Vector3(Input.mousePosition.x / 7800 * (-1), 0, 0));
+            }
+
+        }
+        else
+        {
+            if (player.transform.position.x >= -4.2f)
+            {
+                if (player.transform.position.x <= -5)
+                {
+                    player.transform.position = new Vector3(-5, 0, 0);
+                }
+                //player.transform.Rotate(0,0.1f, 0);
+                player.transform.Translate(new Vector3(-(Screen.width - Input.mousePosition.x) / 7800 * (-1), 0, 0));
+            }
+        }
+        
+    }
+}
--- Molot_Tora/Assets/Scripts/Player.cs
+++ Molot_Tora/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -20,13 +21,26 @@ public class Player : MonoBehaviour
 
     public int bricksNum;
 
+    public string lname;
 
-
+    public Transform MoovePan;
    public int clock ;
    void Start()
     {
+        lname = SceneManager.GetActiveScene().name;
+        if(lname == "Level6")
+        {
+            progressBar.maxValue = 191;
+        }
+        else
+        {
+            progressBar.maxValue = 303;
+        }
+       
+
+
         CoinsScore = PlayerPrefs.GetInt("Coins", CoinsScore);
-        progressBar.maxValue = 303;
+        
 
        for (int i = 0; i < 48; i++)
        {
@@ -41,93 +55,35 @@ public class Player : MonoBehaviour
         ScoreText.text = score.ToString();
 
     }
-    void OnMouseDrag()
-    {
-
-        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //player.transform.position = Vector3.MoveTowards(player.transform.position,  new Vector2(mousePosition.x, player.transform.position.y), 0.2f);
-
-        // Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Vector3 mousePosition  = Input.mousePosition;
-        //mousePosition.x = mousePosition.x > 5f ? 5f : mousePosition.x;
-        //mousePosition.x = mousePosition.x < -5f ? -5f : mousePosition.x;
-        //player.transform.position = new Vector2(mousePosition.x, this.transform.position.y);
-
-
-        // Vector2 mousePosition = new Vector2(Input.mousePosition.x, player.position.y); // переменной записываютьс€ координаты мыши по иксу и игрику
-        // Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваетьс€ переменна€ с координатами мыши
-        // player.position = objPosition; // и собственно объекту записываютьс€ координаты  
-
-
-
-        //Vector2 targetPos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //targetPos.y = 0; // I fixed Y value to zero you may change this to value you want
-        //player.position = targetPos;
-        //if ((Vector2)transform.position != targetPos)
-        //{
-        //    player.position = Vector2.MoveTowards(player.position, targetPos, speed * Time.deltaTime);
-        //
-        //}
-
-        //if (Input.mousePosition.x > Screen.width / 2)
-        //    player.GetComponent<Rigidbody>().MovePosition(new Vector3(Input.mousePosition.x / 100000, 1, 0));
-        //else
-        //    player.GetComponent<Rigidbody>().MovePosition(new Vector3(-(Screen.width - Input.mousePosition.x) / 100000, 1, 0));
-        if (Input.mousePosition.x > Screen.width / 2)
-        {
-            
-           
-            if (player.transform.position.x <= 4.7f)
-            {
-                if (player.transform.position.x >= 5)
-                {
-                    player.transform.position = new Vector3(5,0,0);
-                    
-                }
-               // player.transform.Rotate(0,-0.1f,0);
-                player.transform.Translate(new Vector3(Input.mousePosition.x / 8000*(-1), 0, 0));
-            }
-            
-        }
-        else
-        {
-            if (player.transform.position.x >= -4.2f)
-            {
-                if (player.transform.position.x <= -5)
-                {
-                    player.transform.position = new Vector3(-5, 0, 0);
-                }
-                //player.transform.Rotate(0,0.1f, 0);
-                player.transform.Translate(new Vector3(-(Screen.width - Input.mousePosition.x) /8000 * (-1), 0, 0));
-            }
-        }
-            
-    }
+   
     
     void Move()
     {
+        MoovePan.Translate(Vector3.forward * (-speed));
         player.transform.Translate(Vector3.forward * speed);
         progressBar.value+= 0.25f;
-      //  if (Input.GetKey("a"))
-      //  {
-      //      if (player.transform.position.x >= 5)
-      //      {
-      //          player.transform.position = new Vector3(5, 0, 0);
-      //
-      //      }
-      //
-      //      player.transform.Translate(new Vector3(Input.mousePosition.x / 7500, 0, 0));
-      //
-      //      
-      //  }
-      //  else if (Input.GetKey("d"))
-      //  {
-      //      if (player.transform.position.x <= -5)
-      //      {
-      //          player.transform.position = new Vector3(-5, 0, 0);
-      //      }
-      //      player.transform.Translate(new Vector3(-(Screen.width - Input.mousePosition.x) / 7500, 0, 0));
-      //  }
+        
+        
+        //  if (Input.GetKey("a"))
+        //  {
+        //      if (player.transform.position.x >= 5)
+        //      {
+        //          player.transform.position = new Vector3(5, 0, 0);
+        //
+        //      }
+        //
+        //      player.transform.Translate(new Vector3(Input.mousePosition.x / 7500, 0, 0));
+        //
+        //      
+        //  }
+        //  else if (Input.GetKey("d"))
+        //  {
+        //      if (player.transform.position.x <= -5)
+        //      {
+        //          player.transform.position = new Vector3(-5, 0, 0);
+        //      }
+        //      player.transform.Translate(new Vector3(-(Screen.width - Input.mousePosition.x) / 7500, 0, 0));
+        //  }
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -154,22 +110,35 @@ public class Player : MonoBehaviour
         }
         if (hit.tag == "Gold")
         {
-            bricksNum++;
+            if (lname == "Level6")
+            {
+                bricksNum += 16;
+                clock += 16;
+                for (int i = clock - 16; i < clock; i++)
+                {
+                    hummerBricks[i].gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                bricksNum++;
 
-            hummerBricks[clock].gameObject.SetActive(true);
-            clock++;
-            bricksNum++;
+                hummerBricks[clock].gameObject.SetActive(true);
+                clock++;
+                bricksNum++;
 
-            hummerBricks[clock].gameObject.SetActive(true);
-            clock++;
-            bricksNum++;
+                hummerBricks[clock].gameObject.SetActive(true);
+                clock++;
+                bricksNum++;
 
-            hummerBricks[clock].gameObject.SetActive(true);
-            clock++;
-            bricksNum++;
+                hummerBricks[clock].gameObject.SetActive(true);
+                clock++;
+                bricksNum++;
 
-            hummerBricks[clock].gameObject.SetActive(true);
-            clock++;
+                hummerBricks[clock].gameObject.SetActive(true);
+                clock++;
+            }
+          
         }
         else if (hit.tag == "x3")
         {
